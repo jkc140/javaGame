93a37f1cfd08f5c0291535fe56861c4160bdcb24
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-11-05 11:18:12 -0500

    testing stuff

--- battleShip/src/battleship/BattleShip.java
+++ battleShip/src/battleship/BattleShip.java
@@ -4,21 +4,51 @@
  * and open the template in the editor.
  */
 package battleship;
-import java.awt.*;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
 import javax.swing.*;
 /**
  *
  * @author S331474817
  */
-public class BattleShip {
-    public void draw(){
+public class BattleShip extends JPanel{
+    public static void boardInit(){
         
+        
+    }
+    public void grid(Graphics g){
+        int maxHeight=450;
+        int maxLength=450;
+        int gridSize=10;
+        String testing="";
+        for(int x=0;x<gridSize;x++){
+            //testing=String.valueOf(char(65+x));
+            int val=0+(x*50);
+            g.drawLine(val, 0, val, maxHeight);
+            g.drawLine(0, val,maxLength, val);
+            //g.drawString(, 25, 25);
+        }
+                
+        
+    }
+    public void paintComponent(Graphics g){
+        grid(g);
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrame screen = new JFrame("test");
+        BattleShip ship = new BattleShip();
+        screen.add(ship);
+        screen.setSize(1000,500);
+        screen.setVisible(true);
+        screen.setResizable(false);
     }
     
 }
+
+
+//https://stackoverflow.com/questions/15544549/how-does-paintcomponent-work
