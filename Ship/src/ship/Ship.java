a8cca5b4ff739828a1a31cb7fd2dd14d190573e9
azureblueflare <50378199+azureblueflare@users.noreply.github.com>
2019-11-06 10:03:23 -0500

    Update Ship.java

--- Ship/src/ship/Ship.java
+++ Ship/src/ship/Ship.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ship;
-
+import java.util.Arrays;
 /**
  *
  * @author S331461103
@@ -17,23 +17,34 @@ public class Ship {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         
     }
-    int[][] board = new int[20][20]; 
+     
     private String name;
     private int size;
     private int health;
-    
     private boolean sunk;
+    public String[][] board = returnBoard();
     
     public Ship(int length, String name){
     this.name = name;
     this.size = length;
     this.health = length;
-    this.sunk = false
+    this.sunk = false;
   
     }
+    public static String[][] returnBoard( )  
+{  
+    String[][] board = new String[20][20];
+    for(int x=0;x < 20;x++)
+        {
+            for(int y=0;y < 20;y++)
+        {
+            board[x][y] = ("E");
+        }
+        }
     
-    
+    return board;
+}
 }
