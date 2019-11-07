a5c895959a49c3b93a49a325c409260ac249c12b
J C <>
2019-11-06 23:27:59 -0500

    got graphics and buttons to both show up - when adding graphics must add it last as everything is added on top of it - remove unneeded imports - change boardInit to screenInit as grid class will create the board - make a JPanel that is the grid, so it can be added as a component and get both graphics and button visible - added another good link

--- battleShip/src/battleship/BattleShip.java
+++ battleShip/src/battleship/BattleShip.java
@@ -4,9 +4,6 @@
  * and open the template in the editor.
  */
 package battleship;
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.*;
 
@@ -16,18 +13,17 @@ import javax.swing.*;
  */
 public class BattleShip {
 
-        public JButton bAttack1, bAttack2,bAttack3,bFire; //declaring variables as buttons
+        public static JButton bAttack1, bAttack2,bAttack3,bFire; //declaring variables as buttons
 
 
         public static void buttonConfig(){
 
 
-        screen.add(ship);
-        JButton bAttack1 =new JButton("Normal Attack");
+        bAttack1 =new JButton("Normal Attack");
        /*JButton bAttack2=new JButton("Cross Attack");
         JButton bAttack3=new JButton("2x2 Attack");
        JButton bFire=new JButton("fire");*/
-        bAttack1.setBounds(100, 30,120,50);
+        bAttack1.setBounds(600, 30,120,50);
         //bAttack1.setSize(50, 50);
        // bAttack2.setSize(50, 50);
        /// bAttack3.setSize(50, 50);
@@ -41,13 +37,14 @@ public class BattleShip {
 
 
     }
-    public screenInit(){
+    public static void screenInit(){
       JFrame screen = new JFrame("test");
-      grid gr=new grid();
-      screen.setLayout(null);
+      JPanel test= new grid();
+      //screen.setLayout(null);
       buttonConfig();
+
       screen.add(bAttack1);
-      screen.add(gr);
+      screen.add(test);
 
       screen.setSize(1000,700);
       screen.setVisible(true);
