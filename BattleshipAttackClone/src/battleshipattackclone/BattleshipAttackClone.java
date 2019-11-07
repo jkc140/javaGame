acd93a0e25b62ff78f11b7a2bb909384c99aba5b
S331462374 <S331462374@LDHS-BCT-131400.ad.ocdsb.ca>
2019-11-07 11:26:42 -0500

    creating attack for battleship

--- BattleshipAttackClone/src/battleshipattackclone/BattleshipAttackClone.java
+++ BattleshipAttackClone/src/battleshipattackclone/BattleshipAttackClone.java
@@ -14,16 +14,30 @@ import java.util.Scanner;
 public class BattleshipAttackClone {
 
     /**
-     * @param args the command line arguments
+     * @return 
      */
-    public static void main(String[] args) {
+    public String AttackChoice() {
+        String Choice="";
          Scanner input = new Scanner(System.in);  
         System.out.println("Which attack would you like to choose?");
         String AttackChoice;
         String Instruction = "Cross Attack(1)"+"\n"+"2x2(2)"+"\n"+"Single(3)";
         System.out.println(Instruction);
         AttackChoice = input.nextLine();
-        
+        if ("1".equals(AttackChoice)){
+            Choice="Cross";
+            else if("2".equals(AttackChoice)){
+                    Choice="2x2";
+                }
+                    else{
+                    Choice="Single";
+                    }
+          
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+    
     }
     
 }
