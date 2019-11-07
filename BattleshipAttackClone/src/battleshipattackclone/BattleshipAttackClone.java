d2fbc41d615d4c4ff81defacf2c67494133060dd
S331462374 <S331462374@LDHS-BCT-131400.ad.ocdsb.ca>
2019-11-07 10:48:50 -0500

    creating attack for battleship

---
+++ BattleshipAttackClone/src/battleshipattackclone/BattleshipAttackClone.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleshipattackclone;
+
+/**
+ *
+ * @author S331462374
+ */
+public class BattleshipAttackClone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
