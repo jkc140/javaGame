34d39797efb9257a32367657e0c12c9fdd03481f
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-11-13 11:21:02 -0500

    - adding/getting the health to work

--- battleShip/src/battleshipattackclone/squareAttack.java
+++ battleShip/src/battleshipattackclone/squareAttack.java
@@ -4,13 +4,14 @@
  * and open the template in the editor.
  */
 package battleshipattackclone;
-
+ import ship.*;
 /**
  *
  * @author S331474817
  */
 public class squareAttack extends BattleshipAttackClone{
-        public int[][]squareCoords=new int[4][2];
+       public int[][]squareCoords=new int[5][2];
+        public String[] hitOrMiss=new String[5];
         public void squarePos(){       
         //left
         squareCoords[0][0]=coords[0];
@@ -24,19 +25,41 @@ public class squareAttack extends BattleshipAttackClone{
         //bottom right
         squareCoords[3][0]=coords[0]+1;
         squareCoords[3][1]=coords[0]+1;
+        
+        squareCoords[4][0]=-2;
+        squareCoords[4][1]=-2;
     }
-    public void checkHit( int[][]shipPos){
-        for(int x=0;x<5;x++){
+    public void checkHit( int turn, int[][][][]shipPos,Ship[][]shipList){
+    	int pCheck;
+   	 
+   	 if(turn==0) {
+   		 pCheck=1;
+   	 }
+   	 else {
+   		 pCheck=0;
+   	 }
+    	for(int x=0;x<5;x++){
             for(int y=0;y<5;y++){
                 for(int z=0;z<4;z++){
-                    for(int a=0;a<2;a++){
-                        if(shipPos[x][y]==squareCoords[z][a]){
+                		if(hitOrMiss[x]=="hit") {
+                			break;
+                		}
+                    	if((shipPos[pCheck][0][y][0]==squareCoords[x][0])&&(shipPos[pCheck][y][x][1]==squareCoords[x][1])){
                             System.out.println("Hit");
+                            hitOrMiss[x]="hit";
+                            System.out.println(shipList[pCheck][0].health);
+                            shipList[pCheck][0].health=shipList[pCheck][0].health-1;
+                            System.out.println(shipList[pCheck][0].health);
+                            
+                            if(shipList[pCheck][0].health==0){
+                                System.out.println("Sunk");
+                            }
                         }
                         else{
                             System.out.println("Miss");
+                            hitOrMiss[x]="miss";
                         }
-                    }
+                    
                 }  
             }
         }
