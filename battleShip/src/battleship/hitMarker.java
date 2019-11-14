49fc15dc2fd99edaa78c7c7ddcbe8f3069d9a282
J C <>
2019-11-14 00:00:07 -0500

    comment out unneeded printstatements

--- battleShip/src/battleship/hitMarker.java
+++ battleShip/src/battleship/hitMarker.java
@@ -13,28 +13,28 @@ public class hitMarker extends JPanel{
 
 	public void getInfo(int attackSize, String[] HOM, int[][]coords) {
 		int temp=0;
-		System.out.println("tot attack val: "+ totalAttack);
+		//System.out.println("tot attack val: "+ totalAttack);
 
 		//hit=new String[attackSize];
-		System.out.println("hit length " +hit.length);
+		//System.out.println("hit length " +hit.length);
 		for(int x=0;x<attackSize;x++) {
 			if (hit[0]!=null){
-				System.out.println("adding");
+				//System.out.println("adding");
 				 temp=x+totalAttack;
 			}
 			else {
 				temp=x;
 			}//test
 
-			System.out.println("tot attack val: "+ totalAttack);
+			/*System.out.println("tot attack val: "+ totalAttack);
 			System.out.println("tot attack +x val: "+ (totalAttack+x));
 			System.out.println("drawing x val: "+x);
-			System.out.println("temp: "+temp);
+			System.out.println("temp: "+temp);*/
 
-			System.out.println("HOM: "+HOM[x]);
+			//System.out.println("HOM: "+HOM[x]);
 			hit[(temp)]=HOM[x];
-			System.out.println("hit xTot: "+hit[(temp)]);
-			System.out.println("hit x: "+hit[x]);
+			/*System.out.println("hit xTot: "+hit[(temp)]);
+			System.out.println("hit x: "+hit[x]);*/
 		}
 
 		//attackCoords=new int[tempSizeCoords][2];
@@ -42,13 +42,13 @@ public class hitMarker extends JPanel{
 		for(int x=0;x<attackSize;x++) {
 			temp=x+totalAttack;
 			for(int y=0;y<2;y++) {
-				System.out.println("coords: "+coords[x][y]);
+				//System.out.println("coords: "+coords[x][y]);
 				attackCoords[temp][y]=coords[x][y];
-				System.out.println("coords: "+coords[x][y]);
-				System.out.println("attack Coords: "+attackCoords[temp][y]);
+				/*System.out.println("coords: "+coords[x][y]);
+				System.out.println("attack Coords: "+attackCoords[temp][y]);*/
 			}
-			System.out.println("attack Coords: "+attackCoords[x][0]);
-			System.out.println("attack Coords: "+attackCoords[x][1]);
+			/*System.out.println("attack Coords: "+attackCoords[x][0]);
+			System.out.println("attack Coords: "+attackCoords[x][1]);*/
 		}
 		totalAttack += attackSize;
 
@@ -56,9 +56,9 @@ public class hitMarker extends JPanel{
 	public void drawMarker(Graphics g) {
 		System.out.println("tot attack val: "+ totalAttack);
 		for(int x=0;x<totalAttack;x++) {
-			System.out.println("drawing x val: " +x);
+			/*System.out.println("drawing x val: " +x);
 			System.out.println("hit length " +hit.length);
-			System.out.println(hit[x]);
+			System.out.println(hit[x]);*/
 			if(hit[x].equalsIgnoreCase("hit")){
 				g.setColor(Color.red);;
 
