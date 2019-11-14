f7776ae09cafbe057437c44809f1426b16cb4aa7
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-11-14 09:14:34 -0500

    - making it so that the markers will be saved to each person

--- battleShip/src/battleship/hitMarker.java
+++ battleShip/src/battleship/hitMarker.java
@@ -7,20 +7,20 @@ import javax.swing.*;
 
 
 public class hitMarker extends JPanel{
-	String[] hit = new String [81];
-	int[][] attackCoords = new int [81][2];
-	int totalAttack = 0;
+	String[][] hit = new String [2][81];
+	int[][][] attackCoords = new int [2][81][2];
+	int[] totalAttack= new int[2];
 
-	public void getInfo(int attackSize, String[] HOM, int[][]coords) {
+	public void getInfo(int pTurn,int attackSize, String[] HOM, int[][]coords) {
 		int temp=0;
 		//System.out.println("tot attack val: "+ totalAttack);
 
 		//hit=new String[attackSize];
 		//System.out.println("hit length " +hit.length);
 		for(int x=0;x<attackSize;x++) {
-			if (hit[0]!=null){
+			if (hit[pTurn][0]!=null){
 				//System.out.println("adding");
-				 temp=x+totalAttack;
+				 temp=x+totalAttack[pTurn];
 			}
 			else {
 				temp=x;
@@ -32,7 +32,7 @@ public class hitMarker extends JPanel{
 			System.out.println("temp: "+temp);*/
 
 			//System.out.println("HOM: "+HOM[x]);
-			hit[(temp)]=HOM[x];
+			hit[pTurn][(temp)]=HOM[x];
 			/*System.out.println("hit xTot: "+hit[(temp)]);
 			System.out.println("hit x: "+hit[x]);*/
 		}
@@ -40,33 +40,36 @@ public class hitMarker extends JPanel{
 		//attackCoords=new int[tempSizeCoords][2];
 		temp=0;
 		for(int x=0;x<attackSize;x++) {
-			temp=x+totalAttack;
+			temp=x+totalAttack[pTurn];
 			for(int y=0;y<2;y++) {
 				//System.out.println("coords: "+coords[x][y]);
-				attackCoords[temp][y]=coords[x][y];
+				attackCoords[pTurn][temp][y]=coords[x][y];
 				/*System.out.println("coords: "+coords[x][y]);
 				System.out.println("attack Coords: "+attackCoords[temp][y]);*/
 			}
 			/*System.out.println("attack Coords: "+attackCoords[x][0]);
 			System.out.println("attack Coords: "+attackCoords[x][1]);*/
 		}
-		totalAttack += attackSize;
+              
+                    totalAttack[pTurn] += attackSize;
+                
+		
 
 	}
-	public void drawMarker(Graphics g) {
-		System.out.println("tot attack val: "+ totalAttack);
-		for(int x=0;x<totalAttack;x++) {
+	public void drawMarker(Graphics g, int pTurn) {
+		System.out.println("tot attack val: "+ totalAttack[pTurn]);
+		for(int x=0;x<totalAttack[pTurn];x++) {
 			/*System.out.println("drawing x val: " +x);
 			System.out.println("hit length " +hit.length);
 			System.out.println(hit[x]);*/
-			if(hit[x].equalsIgnoreCase("hit")){
+			if(hit[pTurn][x].equalsIgnoreCase("hit")){
 				g.setColor(Color.red);;
 
 			}
 			else {
 				g.setColor(Color.white);
 			}
-			g.fillOval(attackCoords[x][0]*50, attackCoords[x][1]*50, 50, 50);
+			g.fillOval(attackCoords[pTurn][x][0]*50, attackCoords[pTurn][x][1]*50, 50, 50);
 		}
 	}
 }
