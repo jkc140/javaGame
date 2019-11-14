1360b51406cc5bcdb9c77242b3830327f488bc59
J C <>
2019-11-13 19:24:52 -0500

    - potentially getting hitMarker to saving multiple places to work

--- battleShip/src/battleship/hitMarker.java
+++ battleShip/src/battleship/hitMarker.java
@@ -8,36 +8,56 @@ import javax.swing.*;
 
 public class hitMarker extends JPanel{
 	String[] hit = new String [81];
-	int numAttackPos;
 	int[][] attackCoords = new int [81][2];
 	int totalAttack = 0;
 	
 	public void getInfo(int attackSize, String[] HOM, int[][]coords) {
+		int temp=0;
 		totalAttack += attackSize;
-		int tempSizeHit=HOM.length;
-		int tempSizeCoords=coords.length;
-		numAttackPos=attackSize;
-		hit=new String[tempSizeHit];
-		for(int x=0;x<tempSizeHit;x++) {
-			hit[x]=HOM[x];
+		System.out.println("tot attack val: "+ totalAttack);
+
+		//hit=new String[attackSize];
+		System.out.println("hit length " +hit.length);
+		for(int x=0;x<attackSize;x++) {
+			if (hit[0]!=null){
+				System.out.println("adding");
+				 temp=x+totalAttack;
+			}
+			else {
+				temp=x;
+			}
+			
+			System.out.println("tot attack val: "+ totalAttack);
+			System.out.println("tot attack +x val: "+ (totalAttack+x));
+			System.out.println("drawing x val: "+x);
+			System.out.println("temp: "+temp);
+			
+			System.out.println("HOM: "+HOM[x]);
+			hit[(temp)]=HOM[x];
+			System.out.println("hit xTot: "+hit[(temp)]);
+			System.out.println("hit x: "+hit[x]);
 		}
 		
-		attackCoords=new int[tempSizeCoords][2];
-		for(int x=0;x<tempSizeCoords;x++) {
+		//attackCoords=new int[tempSizeCoords][2];
+		for(int x=0;x<attackSize;x++) {
 			for(int y=0;y<2;y++) {
-				attackCoords[x][y]=coords[x][y];
+				attackCoords[x+totalAttack][y]=coords[x][y];
 			}
 		}
 	}
 	public void drawMarker(Graphics g) {
-		for(int x=0;x<totalAttack;x++) {
-			if(hit[x].equalsIgnoreCase("hit")){
+		System.out.println("tot attack val: "+ totalAttack);
+		for(int x=1;x<=totalAttack;x++) {
+			System.out.println("drawing x val: " +x);
+			System.out.println("hit length " +hit.length);
+			System.out.println(hit[x]);
+			/*if(hit[x].equalsIgnoreCase("hit")){
 				g.setColor(Color.red);;
 				
 			}
 			else {
 				g.setColor(Color.white);
-			}
+			}*/
 			g.fillOval(attackCoords[x][0]*50, attackCoords[x][1]*50, 50, 50);
 		}
 	}
