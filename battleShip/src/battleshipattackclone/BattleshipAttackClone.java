263538e1ba2155e5a7791caf4468ddb2d3f6ab3f
J C <>
2019-11-16 00:43:53 -0500

    removing the changes for the attacking as it was easier/working when it was in the main program

--- battleShip/src/battleshipattackclone/BattleshipAttackClone.java
+++ battleShip/src/battleshipattackclone/BattleshipAttackClone.java
@@ -8,9 +8,6 @@ package battleshipattackclone;
 import java.util.Scanner;
 import javax.swing.*;
 
-import battleship.hitMarker;
-import battleship.shipInit;
-
 
 /**
  *
@@ -21,10 +18,8 @@ public class BattleshipAttackClone { //creating a class called BattleshipAttackC
     /**
      * @return
      */
-    public void AttackChoice() { //create a function called AttackChoice to determine the attack choice of the user <-- it will return a string
+    public String AttackChoice() { //create a function called AttackChoice to determine the attack choice of the user <-- it will return a string
         String Choice=""; //create a function called choice that will hold the input of the user
-        shipInit sI=new shipInit(); //create an object of type shipInit
-        hitMarker hMark=new hitMarker(); //create an object of class hitMarker
         boolean valid=false; //create a variable to determine if the input is valid
         Scanner input = new Scanner(System.in);  //create an object called input of the scanner class
         System.out.println("Which attack would you like to choose?"); //ask the user what attack they would like
@@ -33,32 +28,16 @@ public class BattleshipAttackClone { //creating a class called BattleshipAttackC
         System.out.println(Instruction); //displays the Instruction to the user
         do{ //loops until input is valid
         	AttackChoice = input.nextLine(); //get user input
-            if ("1".equals(AttackChoice)){ //check to see if the user chose cross attack
+            if ("1".equals(AttackChoice)){ //check to see if the user choce cross attack
                 Choice="Cross"; //sets choice as cross
-                crossAttack cAttack = new crossAttack(); //creates a new object called cAttack that is of crossAttack class
-                cAttack.getCoords();//gets the coordinate for the attack (middle coordinate for the cross attack)
-                cAttack.crossPos(); //set the other coordinate for the cross attacks
-                cAttack.checkHit(sI.turn,sI.pos,sI.shipList); //checks to see if the attack hits any of the opponents ships
-                hMark.getInfo(sI.turn,5, cAttack.hitOrMiss, cAttack.crossCoords); //calls function getInfo from hitMarker class to set the information for the markers that has to be drawn
                 valid=true; //sets valid to be true
             }
             else if("2".equals(AttackChoice)){ //same as cross attack, but  for 2x2 (square) attack
                 Choice="2x2";
-                squareAttack sAttack = new squareAttack(); //creates an object of squareAttack called sAttack
-                sAttack.getCoords();//gets the coordinate for the attack (start/base of attack position)
-                sAttack.squarePos();
-                sAttack.checkHit(sI.turn,sI.pos,sI.shipList);
-                hMark.getInfo(sI.turn,4, sAttack.hitOrMiss, sAttack.squareCoords);
                 valid=true;
             }
             else if ("3".equalsIgnoreCase(AttackChoice)){ //same as cross attack, but for single attack
                 Choice="Single";
-                singleAttack singleAttack=new singleAttack(); //creates an object of singleAttack called singleAttack
-                singleAttack.getCoords();
-                singleAttack.singlePos();
-                singleAttack.checkHit(sI.turn,sI.pos,sI.shipList);
-                hMark.getInfo(sI.turn,1, singleAttack.hitOrMiss, singleAttack.singleCoords);
-
                 valid=true;
             }
             else{ //if input is invalid
@@ -66,8 +45,9 @@ public class BattleshipAttackClone { //creating a class called BattleshipAttackC
                 valid=false; //sets valid to false
             }
         }while(valid==false); //loops until valid is true
+        return Choice; //returns user input
     }
-  
+
     public void getCoords(){ //create a function called getCoords to get the coordinates of the base attack
         Scanner sc=new Scanner(System.in); //create an object called sc which is the scanner class
         boolean valid=false;//create a variable to determine if the input is valid
